32c654b8da4140435c7dd600069aebaa237ea6ca
Jean Brito <jean_carlo291@hotmail.com>
2023-10-26 21:53:00 -0400

    Implements search

--- src/App.js
+++ src/App.js
@@ -13,64 +13,8 @@ import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
 import * as React from 'react';
 import makeAutenticate from './api/api';
+import getGame from './api/api';
 
-function sleep(duration) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, duration);
-  });
-}
-
-const topFilms = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-];
 
 function App() {
   const [userInfo, setUserInfo] = useState(null)
@@ -79,6 +23,7 @@ function App() {
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+    console.log(event.currentTarget)
   };
   const handleClose = () => {
     setAnchorEl(null);
@@ -102,31 +47,14 @@ function App() {
   const [options, setOptions] = React.useState([]);
   const loading = open && options.length === 0;
 
-  React.useEffect(() => {
-    let active = true;
 
-    if (!loading) {
-      return undefined;
-    }
+  function searchGame(search){
+    getGame(search).then((result)=>{
+      console.log(result.data.results)
+      setOptions(result.data.results)
+    })
 
-    (async () => {
-      await sleep(1e3); // For demo purposes.
-
-      if (active) {
-        setOptions([...topFilms]);
-      }
-    })();
-
-    return () => {
-      active = false;
-    };
-  }, [loading]);
-
-  React.useEffect(() => {
-    if (!open) {
-      setOptions([]);
-    }
-  }, [open]);
+  }
 
   return (
     <div className="app">
@@ -135,6 +63,11 @@ function App() {
         <div> <Autocomplete
       id="asynchronous-demo"
       sx={{ width: 300 }}
+      onChange={(event, newValue) => {
+        if(newValue){
+          window.alert(newValue.name+" selected!")
+        }
+      }}
       open={opena}
       onOpen={() => {
         setOpen(true);
@@ -142,16 +75,21 @@ function App() {
       onClose={() => {
         setOpen(false);
       }}
-      isOptionEqualToValue={(option, value) => option.title === value.title}
-      getOptionLabel={(option) => option.title}
+      isOptionEqualToValue={(option, value) => option.name === value.name}
+      getOptionLabel={(option) => option.name}
       options={options}
       loading={loading}
       renderInput={(params) => (
         <TextField
           {...params}
-          label="Asynchronous"
+          label="Games"
           size='small'
-          onChange={(e)=>console.log(e.target.value)}
+          onChange={(e)=>searchGame(e.target.value)}
+          sx={{
+            input: {
+              color: "white",
+            }
+          }}
           InputProps={{
             ...params.InputProps,
             endAdornment: (
--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,11 @@
-// const axios = require('axios');
-const axios = null
-export default function makeAutenticate(){
-    
+import axios from "axios"
+
+const instance = axios.create({
+    baseURL: 'https://api.rawg.io/api',
+  });
+
+
+
+export default function getGame(search){
+return instance.get('/games',{ params: {key: "3a27f22286a44b37873aa859b07d8c5a", search } })
 }
